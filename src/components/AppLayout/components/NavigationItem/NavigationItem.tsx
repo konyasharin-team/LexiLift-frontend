972ccfa950558e9be535dcc89a0c210b5704ebb9
Nikolay <kolechka.malyshev.04@gmail.com>
2024-11-16 02:08:17 +0700

    fix capitalize in navigation

--- src/components/AppLayout/components/NavigationItem/NavigationItem.tsx
+++ src/components/AppLayout/components/NavigationItem/NavigationItem.tsx
@@ -68,7 +68,7 @@ export const NavigationItem: FC<INavigationItem> = props => {
         <Flex gap={15} align={'center'} h={'100%'}>
           {props.icon}
           <motion.span variants={textVariants}>
-            <Text fz={18} fw={500} tt={'capitalize'} mt={15} mb={15}>
+            <Text fz={18} fw={500} mt={15} mb={15}>
               {props.text}
             </Text>
           </motion.span>
